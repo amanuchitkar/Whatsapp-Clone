ee4924f7ccdbd2d1eb764f82c01510b600dd35b2
aman uchitkar <amanuchitkar@gmail.com>
2024-03-06 15:36:40 +0530

    Day 4

--- script.js
+++ script.js
@@ -3,28 +3,3 @@ const name = prompt("Enter Your Name to Join..");
 // console.log(name)
 
 socket.emit('new-user-joined', name);
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const messageInput = document.getElementById("message-input");
-//     const sendButton = document.getElementById("send-button");
-//     const chatMessages = document.getElementById("chat-messages");
-
-
-//     sendButton.addEventListener("click", function () {
-//         const messageText = messageInput.value.trim();
-//         if (messageText !== "") {
-//             const messageElement = document.createElement("div");
-//             messageElement.textContent = messageText;
-//             messageElement.classList.add("right", "chat");
-//             chatMessages.appendChild(messageElement);
-//             messageInput.value = "";
-//             chatMessages.scrollTop = chatMessages.scrollHeight;
-//         }
-//     });
-
-//     messageInput.addEventListener("keypress", function (event) {
-//         if (event.key === "Enter") {
-//             sendButton.click();
-//         }
-//     });
-// });
